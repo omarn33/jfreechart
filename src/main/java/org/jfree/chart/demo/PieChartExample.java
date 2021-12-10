464cf5bb2716b26296121d46ef155969bea61a98
omarn33 <omarnaeem333@gmail.com>
2021-12-09 20:19:29 -0600

    Add PieChartExample() method to PieChartExample class

--- src/main/java/org/jfree/chart/demo/PieChartExample.java
+++ src/main/java/org/jfree/chart/demo/PieChartExample.java
@@ -13,3 +13,30 @@ import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.xy.XYDataset;
 
+public class PieChartExample extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+
+    public PieChartExample(String title) {
+        // Add title
+        super(title);
+
+        // Create dataset
+        XYDataset dataset = createDataset();
+
+        // Create chart
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+                "Pie Chart Example", // Chart
+                "Date", // X-Axis Label
+                "Number", // Y-Axis Label
+                dataset);
+
+        //Changes background color
+        XYPlot plot = (XYPlot)chart.getPlot();
+        plot.setBackgroundPaint(new Color(255,228,196));
+
+        // Crate panel
+        ChartPanel panel = new ChartPanel(chart);
+        setContentPane(panel);
+    }
+}
