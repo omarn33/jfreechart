82b01f2a4ca1851af96c6f5bb6bf798cd0d9be0b
omarn33 <omarnaeem333@gmail.com>
2021-12-09 20:22:08 -0600

    Add createDataset() method in PieChartExample class

--- src/main/java/org/jfree/chart/demo/PieChartExample.java
+++ src/main/java/org/jfree/chart/demo/PieChartExample.java
@@ -39,4 +39,49 @@ public class PieChartExample extends JFrame {
         ChartPanel panel = new ChartPanel(chart);
         setContentPane(panel);
     }
+
+    private XYDataset createDataset() {
+        // Create a new dataset
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+
+        // Create a new chart and add appropriate dataset
+        TimeSeries series1 = new TimeSeries("Series1");
+        series1.add(new Day(1, 1, 1890), 50);
+        series1.add(new Day(2, 2, 1890), 40);
+        series1.add(new Day(3, 3, 1890), 45);
+        series1.add(new Day(4, 4, 1890), 30);
+        series1.add(new Day(5, 5, 1890), 50);
+        series1.add(new Day(6, 6, 1890), 45);
+        series1.add(new Day(7, 7, 1890), 60);
+        series1.add(new Day(8, 8, 1890), 45);
+        series1.add(new Day(9, 9, 1890), 55);
+        series1.add(new Day(10, 10, 1890), 48);
+        series1.add(new Day(11, 11, 1890), 60);
+        series1.add(new Day(12, 12, 1890), 45);
+        series1.add(new Day(1, 1, 1891), 65);
+        series1.add(new Day(2, 2, 1891), 45);
+        series1.add(new Day(13, 3, 1891), 55);
+        dataset.addSeries(series1);
+
+        // Create a new chart and add appropriate dataset
+        TimeSeries series2 = new TimeSeries("Series2");
+        series2.add(new Day(1, 1, 1890), 40);
+        series2.add(new Day(2, 1, 1890), 35);
+        series2.add(new Day(3, 1, 1890), 26);
+        series2.add(new Day(4, 1, 1890), 45);
+        series2.add(new Day(5, 1, 1890), 40);
+        series2.add(new Day(6, 1, 1890), 35);
+        series2.add(new Day(7, 1, 1890), 45);
+        series2.add(new Day(8, 1, 1890), 48);
+        series2.add(new Day(9, 1, 1890), 31);
+        series2.add(new Day(10, 1, 1890), 32);
+        series2.add(new Day(11, 1, 1890), 21);
+        series2.add(new Day(12, 1, 1890), 35);
+        series2.add(new Day(13, 1, 1890), 10);
+        series2.add(new Day(14, 1, 1890), 25);
+        series2.add(new Day(15, 1, 1890), 15);
+        dataset.addSeries(series2);
+
+        return dataset;
+    }
 }
