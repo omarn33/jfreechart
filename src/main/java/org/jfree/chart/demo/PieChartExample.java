68fe2efb71cca3592201d148421f973922228269
omarn33 <omarnaeem333@gmail.com>
2021-12-09 20:14:21 -0600

    Add example chart java file

---
+++ src/main/java/org/jfree/chart/demo/PieChartExample.java
@@ -0,0 +1,15 @@
+// Import statements
+package org.jfree.chart.demo;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.swing.ChartPanel;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
+
